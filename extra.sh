7e6fc659a559644a1198008333a4b9d191eb0d3e
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2020-07-02 17:53:50 -0500

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,6 +1,4 @@
-LINES=`fping -a -c1 -t500 -g 172.27.1.1 172.27.1.254 2>&1 | grep --color="never" "min/avg/max" | sed 's/ \+/_/g'`
-
-for lines in $LINES
-do 
-    echo $line | cut -d ' ' -f 
-done 
+NUMNODES=15 
+LINES=`fping -q -c1 -g 10.0.0.2 10.0.0.254 2>&1 | grep -m $NUMNODES --color="never" "min/avg/max" | sed 's/ \+/_/g'`
+IPS=`for line in $LINES; do echo $line | cut -d '_' -f 1; done`
+NUMIPS=`echo $IPS | wc -w`
