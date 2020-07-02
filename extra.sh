e0c6cedde290adbc6dfee5c42e9d9fe698b3a748
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2020-07-02 17:25:55 -0500

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,6 +1,6 @@
-LINES = `fping -a -c1 -t500 -g 172.27.1.1 172.27.1.254 2>&1 | grep --color="never" "min/avg/max"`
+LINES=`fping -a -c1 -t500 -g 172.27.1.1 172.27.1.254 2>&1 | grep --color="never" "min/avg/max" | sed 's/ \+/_/g'`
 
 for lines in $LINES
 do 
-    echo $line | cut -d ' <space>' -f <field number>
+    echo $line | cut -d ' ' -f 
 done 
